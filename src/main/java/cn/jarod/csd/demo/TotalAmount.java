fbcefc9743af18420bed3e2bf5a79a07c5b06358
jarod-jin <kira277@163.com>
2017-09-14 15:47:49 +0800

    随便提交

--- src/main/java/cn/jarod/csd/demo/TotalAmount.java
+++ src/main/java/cn/jarod/csd/demo/TotalAmount.java
@@ -14,34 +14,28 @@ public class TotalAmount {
     }
 
     public double query(String s, String s1) throws ParseException {
-        List<String> monthList = getMonthBetween(s,s1);
-        for (String str : monthList){
-            BudgetRepo br = new BudgetRepo();
+        int monthNum = getMonthSpace(s,s1);
 
-
-        }
         return 0;
     }
 
-    private List<String> getMonthBetween(String minDate, String maxDate) throws ParseException {
-        ArrayList<String> result = new ArrayList<String>();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");//格式化为年月
+    public static int getMonthSpace(String date1, String date2)
+            throws ParseException {
 
-        Calendar min = Calendar.getInstance();
-        Calendar max = Calendar.getInstance();
+        int result = 0;
 
-        min.setTime(sdf.parse(minDate));
-        min.set(min.get(Calendar.YEAR), min.get(Calendar.MONTH), 1);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-        max.setTime(sdf.parse(maxDate));
-        max.set(max.get(Calendar.YEAR), max.get(Calendar.MONTH), 2);
+        Calendar c1 = Calendar.getInstance();
+        Calendar c2 = Calendar.getInstance();
 
-        Calendar curr = min;
-        while (curr.before(max)) {
-            result.add(sdf.format(curr.getTime()));
-            curr.add(Calendar.MONTH, 1);
-        }
+        c1.setTime(sdf.parse(date1));
+        c2.setTime(sdf.parse(date2));
 
-        return result;
+        result = c2.get(Calendar.MONTH) - c1.get(Calendar.MONTH);
+
+        return result == 0 ? 1 : Math.abs(result);
+
     }
+
 }

876b7b805c67b015f10906dcd03ff223ff227c03
jarod-jin <kira277@163.com>
2017-09-15 11:14:55 +0800

    重构2

--- src/main/java/cn/jarod/csd/demo/TotalAmount.java
+++ src/main/java/cn/jarod/csd/demo/TotalAmount.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.List;
 
 import static cn.jarod.csd.demo.DateString.createLocalDate;
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class TotalAmount {
 
@@ -22,33 +23,28 @@ public class TotalAmount {
 
     public double query(String startDateStr, String endDateStr) throws ParseException {
         double total = 0;
-        LocalDate startDate = createLocalDate(startDateStr);
-        LocalDate endDate = createLocalDate(endDateStr);
-        if (startDate.minusDays(1).isBefore(endDate)){
-            List<String> monthList = getMonthBetween(startDate,endDate);
-            if (isInOneMonth(monthList.size())){
-                int d_day = endDate.getDayOfMonth()-startDate.getDayOfMonth() + 1;
-                total +=  getAmountByMonth(startDateStr.substring(0,6),d_day);
-            }
-            else
-            {
-                for (int m = 0 ; m<monthList.size(); m++){
-                    if (monthList.get(m).equals(startDateStr.substring(0,6))) {
-                        int firstMonthDays = startDate.lengthOfMonth() - startDate.getDayOfMonth() + 1;
-                        total += getAmountByMonth(monthList.get(m), firstMonthDays);
-                    }else if (monthList.get(m).equals(endDateStr.substring(0,6))){
-                        int lastMonthDays = endDate.getDayOfMonth();
-                        total += getAmountByMonth(monthList.get(m), lastMonthDays);
-                    }else{
-                        for (Budget b : stubBudgetRepo.findAll()) {
-                            if (b.getMonth().equals(monthList.get(m))){
-                                total +=  b.getAmount();
-                            }
-                        }
+        LocalDate startDate = LocalDate.parse(startDateStr,ofPattern("yyyyMMdd"));
+        LocalDate endDate = LocalDate.parse(endDateStr,ofPattern("yyyyMMdd"));
+        if (startDate.isAfter(endDate)) {
+            return total;
+        }
+        List<String> monthList = getMonthBetween(startDate,endDate);
+        if (isInOneMonth(monthList.size())){
+            total =  getAmountByMonth(startDateStr.substring(0,6),(endDate.getDayOfMonth()-startDate.getDayOfMonth() + 1));
+            return total;
+        }
+        for (String monthStr : monthList){
+            if (monthStr.equals(startDateStr.substring(0,6))) {
+                total += getAmountByMonth(monthStr, (startDate.lengthOfMonth() - startDate.getDayOfMonth() + 1));
+            }else if (monthStr.equals(endDateStr.substring(0,6))){
+                total += getAmountByMonth(monthStr, endDate.getDayOfMonth());
+            }else{
+                for (Budget b : stubBudgetRepo.findAll()) {
+                    if (b.getMonth().equals(monthStr)){
+                        total +=  b.getAmount();
                     }
                 }
             }
-
         }
         return total;
     }
